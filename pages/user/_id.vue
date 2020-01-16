db97ac14c739ea54d4a038b6800aa6b9417e633b
JaxBBLL <showcye@foxmail.com>
2020-01-16 16:45:06 +0800

    build: project

---
+++ pages/user/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <p>{{ $route.params.id }}</p>
+    <nuxt-link to="/user">
+      user
+    </nuxt-link>
+    <div>
+      {{ info.name }}
+      {{ info.age }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+  data () {
+    return {}
+  },
+  async asyncData ({ $axios }) {
+    const data = await $axios.$get('https://api.myjson.com/bins/66s9a')
+    return { info: data }
+  }
+}
+</script>
---
+++ pages/user/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>user list</h1>
+    <nuxt-link to="/user">
+      user
+    </nuxt-link>
+    <ul>
+      <li>
+        <nuxt-link :to="{ name: 'user-id', params: { id: 1 } }">
+          user1
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/user/2">
+          user2
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
