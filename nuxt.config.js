0f448b77bc7b8d6de9cd8eff0a4e43f8669d123c
JaxBBLL <showcye@foxmail.com>
2020-01-17 17:02:35 +0800

    update port

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,7 +70,7 @@ export default {
     extend(config, ctx) {}
   },
   server: {
-    port: 3000, // default: 3000
+    port: 9527, // default: 3000
     host: '0.0.0.0' // default: localhost,
   }
 }
